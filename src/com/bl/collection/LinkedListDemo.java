44404e0bc44ac723de4fe9dfe9bf7e99a0b9917a
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-08-05 00:06:18 +0530

    Added examples for linled list concepts

---
+++ src/com/bl/collection/LinkedListDemo.java
@@ -0,0 +1,21 @@
+package com.bl.collection;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+	public static void main(String args[]) {
+
+		LinkedList<String> al = new LinkedList<String>();
+		al.add("Ravi");
+		al.add("Vijay");
+		al.add("Ravi");
+		al.add("Ajay");
+
+		Iterator<String> itr = al.iterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+	}
+
+}
