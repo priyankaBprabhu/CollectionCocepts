27b137fcca0f971484e2620740ffa2cc83261015
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-08-05 00:21:39 +0530

    Added examples for Tree set methods

---
+++ src/com/bl/collection/TreeSetAccessMethods.java
@@ -0,0 +1,24 @@
+package com.bl.collection;
+
+import java.util.TreeSet;
+
+public class TreeSetAccessMethods {
+	public static void main(String args[]) {
+		TreeSet<String> set = new TreeSet<String>();
+		set.add("A");
+		set.add("B");
+		set.add("C");
+		set.add("D");
+		set.add("E");
+		System.out.println("Initial Set: " + set);
+
+		System.out.println("Reverse Set: " + set.descendingSet());
+
+		System.out.println("Head Set: " + set.headSet("C", true));
+
+		System.out.println("SubSet: " + set.subSet("A", false, "E", true));
+
+		System.out.println("TailSet: " + set.tailSet("C", false));
+	}
+
+}
